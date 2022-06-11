90e80f7f64bafaaa64c9a2878b8edc25923f6024
melkerydman <melkerrydman@gmail.com>
2022-06-11 18:16:37 +0200

    Incorporated react router passing params through url instead of props

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import { SpecificServicePage } from "./Pages";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="App">
-      <SpecificServicePage salonType="hair" />
+      <Routes>
+        <Route path="/services/:service" element={<SpecificServicePage />} />
+      </Routes>
       <h1>Hello world</h1>
     </div>
   );
--- src/Pages/SpecificServicePage/index.jsx
+++ src/Pages/SpecificServicePage/index.jsx
@@ -1,28 +1,33 @@
 import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
-// This page should import the ListView an fill it with data based on salonType
-// Should get passed a "salonType"
-const SpecificServicePage = ({ salonType }) => {
+// This page should import the ListView an fill it with data based on url param
+const SpecificServicePage = () => {
   const [salons, setSalons] = useState([]);
+  const params = useParams();
+
   useEffect(() => {
     const getSalons = async () => {
       try {
-        const data = await fetch("./data.json");
+        const data = await fetch("/data.json");
         const json = await data.json();
 
-        // Get all salons where their service matches the prop passed i.e. hair salons
-        const filteredSalons = json.salons.filter(
-          (salon) => salon.service === salonType
-        );
+        // Get all salons where their service matches the param from url i.e. hair
+        const filteredSalons = json.salons.filter((salon) => {
+          return salon.service === params.service;
+        });
+
+        if (filteredSalons.length === 0) {
+          console.log("No match, should redirect to page cannot be found");
+        }
         setSalons(filteredSalons);
       } catch (err) {
         console.log(err);
       }
     };
     getSalons();
-  }, []);
+  }, [params]);
 
-  console.log(salons);
   return (
     <ul>
       {salons.map((salon, index) => {
